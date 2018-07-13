55808035d51e3bdbabcda748da6c5dfd6e8a97df
Vineet Garg <vineet2garg@mail.com>
2018-07-13 16:14:37 +1000

    Adding Security Module

--- src/main/java/com/springboot/microservice/poll/model/Choice.java
+++ src/main/java/com/springboot/microservice/poll/model/Choice.java
@@ -1,10 +1,58 @@
 package com.springboot.microservice.poll.model;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "choices")
 public class Choice {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank
+	@Size(max = 40)
+	private String text;
+
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "poll_id", nullable = false)
+	private Poll poll;
+
+	public Choice() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Poll getPoll() {
+		return poll;
+	}
+
+	public void setPoll(Poll poll) {
+		this.poll = poll;
+	}
 
 }
